d1e86af49e2fe57616404a2ec48843b65a2faf90
Eric <eric@simontonsoftware.com>
2018-08-17 12:15:16 -0400

    chore: `yarn prettier-all`

--- projects/s-js-utils/karma.conf.js
+++ projects/s-js-utils/karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -32,6 +32,6 @@ module.exports = function (config) {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox', '--disable-gpu']
       }
-    },
+    }
   });
 };
