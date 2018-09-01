7ad44b73bd3d3253c47152751413f79e02008f16
Eric <eric@simontonsoftware.com>
2018-09-01 13:54:49 -0400

    chore: document `Deferred` and the misc utils

--- projects/s-js-utils/src/lib/deferred.ts
+++ projects/s-js-utils/src/lib/deferred.ts
@@ -1,3 +1,16 @@
+/**
+ * An object you can use to create a promise and resolve/reject it later.
+ *
+ * ```ts
+ * const deferred = new Deferred<string>();
+ *
+ * // later
+ * deferred.resolve('a nice value'); // causes `deferred.promise` to resolve
+ *
+ * // or
+ * deferred.reject('a bad value'); // causes `deferred.promise` to reject
+ * ```
+ */
 export class Deferred<T> {
   promise: Promise<T>;
   resolve!: (value?: PromiseLike<T> | T) => void;
--- projects/s-js-utils/src/lib/misc-utils.ts
+++ projects/s-js-utils/src/lib/misc-utils.ts
@@ -1,9 +1,29 @@
 import { isUndefined } from "micro-dash";
 
+/**
+ * Checks if `value` is anything other than `undefined`.
+ *
+ * ```ts
+ * isDefined({ a: 1 }); // true
+ * isDefined(false); // true
+ * isDefined(null); // true
+ * isDefined(undefined); // false
+ * ```
+ */
 export function isDefined(value: any) {
   return !isUndefined(value);
 }
 
+/**
+ * Rounds `value` to the nearest multiple of `multiple`.
+ *
+ * ```ts
+ * roundToMultiple(13, 5); // 15
+ * roundToMultiple(2, 4.8); // 4
+ * roundToMultiple(3, -4); // -3
+ * roundToMultiple(0.5, 1.6); // 1.5
+ * ```
+ */
 export function roundToMultipleOf(multiple: number, value: number) {
   return Math.round(value / multiple) * multiple;
 }
