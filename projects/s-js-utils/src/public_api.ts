e9b5a3dcf4c7de272b347f3c82b3befec2290826
Eric <8042088+ersimont@users.noreply.github.com>
2018-09-15 14:07:10 -0400

    feat: add `createBuilder()`
    BREAKING CHANGE: `createBuilder()` replaces `TestFactorySuperclass`

--- projects/s-js-utils/src/public_api.ts
+++ projects/s-js-utils/src/public_api.ts
@@ -3,10 +3,10 @@
  */
 
 export * from "./lib/callable-object";
+export * from "./lib/create-builder";
 export * from "./lib/deferred";
 export * from "./lib/is-defined";
 export * from "./lib/round-to-multiple-of";
 export * from "./lib/sleep";
-export * from "./lib/test-factory";
 export * from "./lib/time-utils";
 export * from "./lib/to-csv";
