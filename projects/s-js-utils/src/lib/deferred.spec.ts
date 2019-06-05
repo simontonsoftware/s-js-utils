977c5b46e47a9b68507f280f4388dcc3a37a1e85
ersimont <8042088+ersimont@users.noreply.github.com>
2019-06-05 19:40:59 -0400

    feat: improve typing of `Deferred.resolve`

--- projects/s-js-utils/src/lib/deferred.spec.ts
+++ projects/s-js-utils/src/lib/deferred.spec.ts
@@ -1,7 +1,18 @@
+import { expectSingleCallAndReset } from "s-ng-dev-utils";
 import { Deferred } from "./deferred";
 
 describe("Deferred", () => {
-  describe(".resolve", () => {
+  describe(".resolve()", () => {
+    it("has good typing", () => {
+      new Deferred<void>().resolve();
+      new Deferred<string>().resolve("hi");
+      new Deferred<object>().reject();
+      new Deferred<number>().reject("bye");
+
+      // the test is just that the above compiles w/out errors
+      expect().nothing();
+    });
+
     it("uses the provided value", async () => {
       const deferred = new Deferred<number>();
       deferred.resolve(42);
@@ -20,7 +31,7 @@ describe("Deferred", () => {
     });
   });
 
-  describe(".reject", () => {
+  describe(".reject()", () => {
     it("uses the provided value", async () => {
       const deferred = new Deferred<number>();
       deferred.reject("some error");
@@ -34,16 +45,16 @@ describe("Deferred", () => {
 
     it("runs error callback on the micro queue", async () => {
       const deferred = new Deferred<boolean>();
-      let rejected = false;
-      deferred.promise.catch(() => (rejected = true));
+      const spy = jasmine.createSpy();
+      deferred.promise.catch(spy);
 
       deferred.reject();
-      expect(rejected).toBe(false);
+      expect(spy).not.toHaveBeenCalled();
       try {
         await deferred.promise;
         fail("should not reach here");
       } catch {
-        expect(rejected).toBe(true);
+        expectSingleCallAndReset(spy, undefined);
       }
     });
   });
