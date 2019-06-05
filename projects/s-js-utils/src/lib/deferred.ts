5ae5e68529f155dae88d14ce1ace75d7c6789f74
ersimont <8042088+ersimont@users.noreply.github.com>
2019-06-05 19:40:59 -0400

    feat: improve typing of `Deferred.resolve`
    BREAKING CHANGE: To call `Deferred.resolve()` with no arguments, you must declare it to be a `Deferred<void>`

--- projects/s-js-utils/src/lib/deferred.ts
+++ projects/s-js-utils/src/lib/deferred.ts
@@ -13,7 +13,7 @@
  */
 export class Deferred<T> {
   promise: Promise<T>;
-  resolve!: (value?: PromiseLike<T> | T) => void;
+  resolve!: (value: PromiseLike<T> | T) => void;
   reject!: (reason?: any) => void;
 
   constructor() {
